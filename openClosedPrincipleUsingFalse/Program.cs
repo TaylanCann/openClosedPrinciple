46512eba4845697c280e8e8a20185149b1640dfd
TaylanCann <taylancanh@gmail.com>
2022-02-08 18:46:50 +0300

    Add project files.

---
+++ openClosedPrincipleUsingFalse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace openClosedPrincipleUsingFalse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
